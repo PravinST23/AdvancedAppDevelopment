3ffb248c31eb4cf03d760da3cb4d877c1199a4fd
PravinST23 <118874116+PravinST23@users.noreply.github.com>
2024-07-18 17:30:57 +0530

    Delete Backend/Day 9 directory

--- Backend/Day 9/src/main/java/com/britcertify/pravin/utils/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.britcertify.pravin.utils;
-
-public class UserNotFoundException extends Exception {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
